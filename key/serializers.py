eade37e408c5234ae33c0841c776abbf3e8fa3a8
anuanaghavm <anaghavm2019@gmail.com>
2024-03-14 15:15:35 +0530

    update serializers.py

--- key/serializers.py
+++ key/serializers.py
@@ -4,4 +4,4 @@ from .models import key_diffrentiates
 class key_diffrentiatesSerializer(serializers.ModelSerializer):
     class Meta:
         model = key_diffrentiates
-        fields = ['title', 'subtitle', 'icon', 'tagline']
+        fields = [ 'icon', 'tagline']
