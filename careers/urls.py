126f9f2dcf1d6845e8f03231fa39c1dfe4376bd7
anuanaghavm <anaghavm2019@gmail.com>
2024-03-21 18:07:17 +0530

    update header

--- careers/urls.py
+++ careers/urls.py
@@ -3,6 +3,6 @@ from careers import views
 
 
 urlpatterns = [
-    path('career-page/', views.CareerPageRetrieveUpdateView.as_view(), name='career-page'),
+    path('header/career-page/', views.CareerPageRetrieveUpdateView.as_view(), name='career-page'),
     path('career-submissions/', views.CareerSubmissionListCreateView.as_view(), name='career-submissions'),
 ]
--- header/models.py
+++ header/models.py
@@ -6,6 +6,7 @@ class HomeHeader(models.Model):
     subtitle = models.CharField(max_length=200)
     description = models.TextField()
     image = models.ImageField(upload_to='header_images/')
+    url = models.URLField()
     alt_img_text = models.TextField(max_length=300, null=True, blank=True)
     alt_img_title = models.TextField(max_length=300, null=True, blank=True)
     alt_img_Caption = models.TextField(max_length=300, null=True, blank=True)
