7531f6ac95c91e7f2ce3d112b436cb515b6caaef
anuanaghavm <anaghavm2019@gmail.com>
2024-05-01 14:41:55 +0530

    new api servicetitle

--- service_card/models.py
+++ service_card/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
-
 class ServicesCard(models.Model):
     title = models.CharField(max_length=100)
     icon = models.ImageField(upload_to='Services_Card/')
     description = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.title
+    
+class ServiceTitle(models.Model):
+    title = models.CharField(max_length=100)
 
     def __str__(self):
         return self.title
