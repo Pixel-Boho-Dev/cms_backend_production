d8ee9a441dcb04b5c7a673e504ff6522d825a7db
theertha09 <161118217+theertha09@users.noreply.github.com>
2024-05-03 13:34:20 +0530

    .

--- aboutus/serializers.py
+++ aboutus/serializers.py
@@ -3,7 +3,7 @@ from rest_framework.exceptions import ValidationError
 from django.db import models
 
 
-from .models import AboutPageSection,OurStory,Milestone,OurTeam,WhatWeAre,Certifications,MetaTagsAbout,CertificateTitle,OurTeamTitle,MilestoneTitle,WhatWeAreTitle,OurstoryTitle
+from .models import AboutPageSection,OurStory,Milestone,OurTeam,WhatWeAre,Certifications,MetaTagsAbout,CertificateTitle,OurTeamTitle,MilestoneTitle,WhatWeAreTitle
 
 from .models import AboutPageSection,OurStory,Milestone,OurTeam,WhatWeAre,Certifications,MetaTagsAbout,CertificateTitle,OurTeamTitle,WhatWeAreTitle,MilestoneTitle
 
@@ -22,10 +22,6 @@ class OurStorySerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-class OurstoryitleSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = OurstoryTitle
-        fields = '__all__'
 
 class MilestoneSerializer(serializers.ModelSerializer):
     class Meta:
