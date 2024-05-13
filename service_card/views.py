e753a564382ffde3a8131f1215f4e12e05c3c0ae
theertha09 <161118217+theertha09@users.noreply.github.com>
2024-05-13 12:57:56 +0530

    .

--- service_card/views.py
+++ service_card/views.py
@@ -6,11 +6,15 @@ from .serializers import ServicesCardSerializer,ServiceTitleSerializer
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-class ServicesCardListCreateView(generics.ListCreateAPIView):
+class ServicesCardCreateView(generics.ListCreateAPIView):
     queryset = ServicesCard.objects.all()
     serializer_class = ServicesCardSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
     authentication_classes = [JWTAuthentication]
+
+class servicesCardListView(generics.ListCreateAPIView):
+    queryset = ServicesCard.objects.all()
+    serializer_class = ServicesCardSerializer
 
 class ServicesCardRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = ServicesCard.objects.all()
