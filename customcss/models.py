446ff9c5d1266aff18266bcec86c898dc4539915
anuanaghavm <anaghavm2019@gmail.com>
2024-05-23 16:28:49 +0530

    update

--- customcss/models.py
+++ customcss/models.py
@@ -1,11 +1,7 @@
 from django.db import models
 
 class HomeHeaderCustom(models.Model):
-    title = models.TextField()
-    image = models.ImageField(upload_to="Customcss_home")
-    sub_title = models.TextField()
-    description = models.TextField()
-    link = models.URLField()
+    text = models.TextField()
 
     def __str__(self):
         return self.title
