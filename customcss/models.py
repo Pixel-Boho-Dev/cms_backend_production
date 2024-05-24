e0d50dc38182cb245b06020167c2531d5a62db22
Ezer Developers <KFITOFFICE@gmail.com>
2024-05-24 10:38:59 +0530

    .

--- customcss/models.py
+++ customcss/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 
+# models for homeheader
+
 class HomeHeaderCustom(models.Model):
     text = models.TextField()
 
     def __str__(self):
         return self.title
+    
+#models fo aboutpagesection
 
 class AboutPageSectionCustom(models.Model):
     text = models.TextField()
@@ -12,6 +16,15 @@ class AboutPageSectionCustom(models.Model):
     def __str__(self):
         return self.title
 
+#models fo ourstorysection
+
+class ourstoryCustom(models.Model):
+    text = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+#models for servicecard
 
 class ServicecardsCustom(models.Model):
     text = models.TextField()
