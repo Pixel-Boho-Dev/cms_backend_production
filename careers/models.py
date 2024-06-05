f7a63f046bd03c4d177603eb56316ff400f439a8
anuanaghavm <anaghavm2019@gmail.com>
2024-06-05 14:51:49 +0530

    new slug career page

--- careers/models.py
+++ careers/models.py
@@ -1,10 +1,34 @@
 from django.db import models
+from django.utils.text import slugify
+
 
 #models for careerpage
 class CareerPage(models.Model):
     title = models.CharField(max_length=100)
     header_image = models.ImageField(upload_to='career_page')
     description = models.TextField()
+    slug = models.CharField(max_length=200, unique=True, blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug or CareerPage.objects.filter(slug=self.slug).exclude(pk=self.pk).exists():
+            self.slug = self._generate_unique_slug()
+        super().save(*args, **kwargs)
+
+    def _generate_unique_slug(self):
+        base_slug = slugify(self.title) if self.title else "career-page-section"
+        slug = base_slug
+        counter = 1
+        while CareerPage.objects.filter(slug=slug).exclude(pk=self.pk).exists():
+            slug = f"{base_slug}-{counter}"
+            counter += 1
+        return slug
+
+    def __str__(self):
+        return self.title 
+
+    def get_absolute_url(self):
+        return f"/career/{self.slug}/"
+
     
 #models for careersubmission    
 class CareerSubmission(models.Model):
