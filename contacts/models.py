bafce9a3b6efb8270010d9fff1292ec0d146019c
anuanaghavm <anaghavm2019@gmail.com>
2024-06-05 13:48:10 +0530

    new keyword slug

--- contacts/models.py
+++ contacts/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils.text import slugify
+
 
 # Header of the contact page  
 class ContactHeader(models.Model):
@@ -10,6 +12,27 @@ class ContactHeader(models.Model):
     alt_img_title = models.TextField(max_length=300, null=True, blank=True)
     alt_img_Caption = models.TextField(max_length=300, null=True, blank=True)
     alt_img_description = models.TextField(max_length=300, null=True, blank=True)
+    slug = models.CharField(max_length=200, unique=True, blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug or ContactHeader.objects.filter(slug=self.slug).exclude(pk=self.pk).exists():
+            self.slug = self._generate_unique_slug()
+        super().save(*args, **kwargs)
+
+    def _generate_unique_slug(self):
+        base_slug = slugify(self.header_title ) if self.header_title  else "contact header"
+        slug = base_slug
+        counter = 1
+        while ContactHeader.objects.filter(slug=slug).exclude(pk=self.pk).exists():
+            slug = f"{base_slug}-{counter}"
+            counter += 1
+        return slug
+
+    def __str__(self):
+        return self.header_title 
+
+    def get_absolute_url(self):
+        return f"/about/{self.slug}/"
 
 # model for section in contact page if needed
 class Section(models.Model):
