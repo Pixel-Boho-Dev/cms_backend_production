653b9b1ceded3e2a832dbdaa718334298467b438
theertha09 <161118217+theertha09@users.noreply.github.com>
2024-06-26 14:20:40 +0530

    databse changed

--- cmsproject/settings.py
+++ cmsproject/settings.py
@@ -120,11 +120,14 @@ WSGI_APPLICATION = 'cmsproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cms_backend' ,
+        'USER' : 'root' ,
+        'PASSWORD' : '' ,
+        'HOST' : 'localhost' ,
+        'PORT' : '3306' ,
     }
 }
-
 
 # uncomment this in production
 
