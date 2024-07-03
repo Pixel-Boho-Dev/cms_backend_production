57b358d01072a303adb698b6367ea6a4bd3f17ee
anuanaghavm <anaghavm2019@gmail.com>
2024-07-03 16:55:25 +0530

    update service_card

--- service_card/models.py
+++ service_card/models.py
@@ -5,6 +5,7 @@ class ServicesCard(models.Model):
     title = models.CharField(max_length=100)
     icon = models.ImageField(upload_to='Services_Card/')
     description = models.CharField(max_length=500)
+    link = models.URLField(max_length=1000)
 
     def __str__(self):
         return self.title
