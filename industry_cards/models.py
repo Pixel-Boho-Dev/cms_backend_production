bef39bbae647237246d2654b756bcd467b75fc58
anuanaghavm <anaghavm2019@gmail.com>
2024-07-05 13:23:51 +0530

    update industry_cards

--- industry_cards/models.py
+++ industry_cards/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+
 
 #models for industrycards
 class IndustryCard(models.Model):
     title = models.CharField(max_length=100)
     description =models. TextField()
     image = models.ImageField(upload_to='industry_images/')
+    order_by = models.IntegerField(validators=[MinValueValidator(0)], unique=True)
     alt_img_text = models.TextField(max_length=300, null=True, blank=True)
     alt_img_title = models.TextField(max_length=300, null=True, blank=True)
     alt_img_caption = models.TextField(max_length=300, null=True, blank=True)
