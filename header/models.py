dcff2fb377a5a30e0921fc3392e2d81d06a4ede8
anuanaghavm <anaghavm2019@gmail.com>
2024-07-16 15:19:41 +0530

    Removed slug in homepage

--- header/models.py
+++ header/models.py
@@ -23,37 +23,8 @@ class HomeHeader(models.Model):
     alt_img_title = models.TextField(max_length=300, null=True, blank=True)
     alt_img_Caption = models.TextField(max_length=300, null=True, blank=True)
     alt_img_description = models.TextField(max_length=300, null=True, blank=True)
-    slug = models.CharField(max_length=200, unique=True, blank=True, null=True)  # This field stores only the slug
-
-    def save(self, *args, **kwargs):
-        # Use the provided slug, if available and valid
-        if self.slug:
-            # Extract the path component from the provided URL
-            parsed_url = urlparse(self.slug)
-            path = parsed_url.path
-            # Modify the slug to include only the path component
-            self.slug = path
-
-        # If no slug is provided, generate slug based on the title
-        if not self.slug:
-            self.slug = generate_valid_slug(self.title)
-
-        # Ensure the slug is unique
-        self.slug = self._generate_unique_slug(self.slug)
-
-        super().save(*args, **kwargs)
-
-    def _generate_unique_slug(self, base_slug):
-        slug = base_slug
-        counter = 1
-        while HomeHeader.objects.filter(slug=slug).exists():
-            slug = f"{base_slug}-{counter}"
-            counter += 1
-        return slug
     
     def __str__(self):
         return self.title
 
-    def get_absolute_url(self):
-        # Return the complete URL including the base URL
-        return f"https://example.com/home/{self.slug}/"
+
