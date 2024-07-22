0eef808d7ad7677f1aae3ed0cdfced4b3392ee6f
anuanaghavm <anaghavm2019@gmail.com>
2024-07-22 11:12:05 +0530

    update settings.py

--- cmsproject/settings.py
+++ cmsproject/settings.py
@@ -196,8 +196,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 12  # Default page size
+    'DEFAULT_PAGINATION_CLASS': None  # This should disable pagination globally
 }
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
